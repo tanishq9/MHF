88091c962156769c968a35614069f6bef110b812
Tanishq <tanishqsaluja18@gmail.com>
2019-10-08 18:01:44 +0530

    all feat added

--- stocktaking.js
+++ stocktaking.js
@@ -2,6 +2,35 @@ $(document).ready(function(){
     
     console.log('Document loaded');
     
+    var button=$('#reportbtn');
+    var amount=$('#amt');
+    var pan=$('#pannum');
+    var email=$('#email');
+
+    button.click(function(){
+        console.log('Button clicked.');
+        console.log(amount.val());
+        console.log(pan.val());
+        console.log(email.val());
+        var obj={
+            amt:amount.val(),
+            pn:pan.val(),
+            em:email.val()
+        };
+        if(amount.val()=='' || pan.val()=='' || email.val()==''){
+            console.log('Empty Field(s)');
+            myFunction('Empty Fields(s)');
+        }else{
+            myFunction('Email Send');
+            $.post(
+                '/report',
+                obj,
+                function(data){
+                    console.log('Email send.');
+                }
+            )
+        }
+    });
     AOS.init({
         easing: 'ease', 
         duration: 1800,
@@ -9,3 +38,10 @@ $(document).ready(function(){
     });
 
 });
+
+function myFunction(data) {
+    var x = document.getElementById("snackbar");
+    x.textContent = data
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
